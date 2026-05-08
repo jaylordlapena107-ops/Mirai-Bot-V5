9058445b01e1eb09ea5dc17a6d194016841cf674
jaylordlapena107-ops <jaylordlapena107@gmail.com>
2026-05-08 20:33:23 +0800

    Update leaveNoti.js

--- modules/events/leaveNoti.js
+++ modules/events/leaveNoti.js
@@ -3,13 +3,12 @@ module.exports.config = {
     eventType: ["log:unsubscribe"],
     version: "3.0.0",
     credits: "Ranz | Edited",
-    description: "Notify when a user leaves the group"
+    description: "Silent leave system"
 };
 
 module.exports.run = async function ({
     api,
-    event,
-    Users
+    event
 }) {
 
     try {
@@ -23,58 +22,8 @@ module.exports.run = async function ({
         if (leftID == api.getCurrentUserID())
             return;
 
-        // Prevent duplicate message
-        if (!global.leaveCooldown)
-            global.leaveCooldown = new Set();
-
-        const cooldownKey =
-            `${threadID}_${leftID}`;
-
-        if (
-            global.leaveCooldown.has(cooldownKey)
-        ) return;
-
-        global.leaveCooldown.add(cooldownKey);
-
-        setTimeout(() => {
-            global.leaveCooldown.delete(cooldownKey);
-        }, 3000);
-
-        const moment =
-            require("moment-timezone");
-
-        const time =
-            moment
-            .tz("Asia/Manila")
-            .format("hh:mm A • MMM D YYYY");
-
-        const name =
-            global.data.userName.get(leftID)
-            || await Users.getNameUser(leftID);
-
-        const authorName =
-            event.author == leftID
-            ? null
-            : await Users.getNameUser(event.author);
-
-        const status =
-            event.author == leftID
-            ? "Left the group"
-            : `Removed by ${authorName}`;
-
-        const msg =
-`╭───────────────⭓
-│ 👋 MEMBER LEFT
-├───────────────⭔
-│ 👤 Name: ${name}
-│ 📌 Status: ${status}
-│ 🕒 Time: ${time}
-╰───────────────⭓`;
-
-        return api.sendMessage(
-            msg,
-            threadID
-        );
+        // No leave message
+        return;
 
     } catch (e) {
 
